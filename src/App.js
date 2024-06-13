afa2dd3c6d88e54aeee73aa6a478c0d33bb320a2
aamirkhan829055 <aamirkhanhnd12@gmail.com>
2024-06-13 20:51:57 +0530

    new file react-js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import "./App.scss";
+function App() {
+  return (
+    <div className="main-container">
+      <span className="title " id="arbajbm">AAmir khan 1</span>
+      <span className="title title-1" id="arbajbp">Arbaj khan 2</span>
+      <span className="title title-2" id="arbajbq">Sharukh khan 3
+      </span>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  plugins: ["img:images"]
+}
