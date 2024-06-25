a01b3b1652958c6f95a3ca34a0f6283446ab7666
aamirkhan829055 <aamirkhanhnd12@gmail.com>
2024-06-25 20:36:45 +0530

    img add

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,26 +9,24 @@ function Header() {
     return (
         <header className="bg-[#2ec119]">
             <div className="container mx-auto ">
-                <div className="flex justify-between items-center py-3">
-                    <a href="#" className="text-white text-[20px] ">logo</a>
+                <div className="flex justify-between items-center ">
+                    <a href="#" className="text-white text-[20px] inline-block font-bold"><img src={`${process.env.PUBLIC_URL}images/logo.png`} alt="Logo" /></a>
                     <ul>
-                        <li className="inline-block px-3 relative">
-                            <a href="#" className="text-white capitalize font-bold" onClick={() => setToggleDrop(!toggleDrop)} >Home</a>
+                        <li className="inline-block px-3 py-3 relative list-dropdown">
+                            <a href="#" className="text-white capitalize font-bold " onClick={() => setToggleDrop(!toggleDrop)} >Home</a>
                             {toggleDrop && (
-                                <ul className="absolute mt-4 font-bold bg-[#2ec119] px-4">
-                                    <li><a href="#" className="text-block py-2 inline-block text-white">Weocde</a></li>
-                                    <li><a href="#" className="text-block py-2 inline-block text-white">coaching</a></li>
-                                    <li><a href="#" className="text-block py-2 inline-block text-white">institute</a></li>
-                                    <li><a href="#" className="text-block py-2 inline-block text-white">Comfygen</a></li>
+                                <ul className="absolute mt-3 font-bold bg-[#2ec119]">
+                                    <li className="list-dropdown"><a href="#" className="text-block p-2 inline-block text-white">Weocde</a></li>
+                                    <li className="list-dropdown"><a href="#" className="text-block p-2 inline-block text-white">coaching</a></li>
+                                    <li className="list-dropdown"><a href="#" className="text-block p-2 inline-block text-white">institute</a></li>
+                                    <li className="list-dropdown"><a href="#" className="text-block p-2 inline-block text-white">Comfygen</a></li>
                                 </ul>
                             )}
-
-
                         </li>
-                        <li className="inline-block px-3"><a href="#" className="text-white capitalize font-bold">About</a></li>
-                        <li className="inline-block px-3"><a href="#" className="text-white capitalize font-bold">Services</a></li>
-                        <li className="inline-block px-3"><a href="#" className="text-white capitalize font-bold">Project</a></li>
-                        <li className="inline-block px-3"><a href="#" className="text-white capitalize font-bold">Comtect</a></li>
+                        <li className="inline-block px-3 py-3 list-dropdown"><a href="#" className="text-white capitalize font-bold">About</a></li>
+                        <li className="inline-block px-3 py-3 list-dropdown"><a href="#" className="text-white capitalize font-bold">Services</a></li>
+                        <li className="inline-block px-3 py-3 list-dropdown"><a href="#" className="text-white capitalize font-bold">Project</a></li>
+                        <li className="inline-block px-3 py-3 list-dropdown"><a href="#" className="text-white capitalize font-bold">Contect</a></li>
                     </ul>
                 </div>
             </div>
